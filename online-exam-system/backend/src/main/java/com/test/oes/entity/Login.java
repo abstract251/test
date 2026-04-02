fe3b4889a49a9a4cee02a9f427e86048ae4f2d81
管维康 <2671661842@qq.com>
2026-04-02 22:51:23 +0800

    创建了login和admin，teacher，student三个实体类

---
+++ online-exam-system/backend/src/main/java/com/test/oes/entity/Login.java
@@ -0,0 +1,13 @@
+package com.test.oes.entity;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//登录实体类
+public class Login {
+    private String username;
+    private String password;
+
+}
---
+++ online-exam-system/backend/src/main/java/com/test/oes/entity/Student.java
@@ -0,0 +1,33 @@
+package com.test.oes.entity;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//学生实体类
+public class Student {
+    private String studentId;
+
+    private String studentName;
+
+    private String grade;
+
+    private String major;
+
+    private String clazz;
+
+    private String institute;
+
+    private String tel;
+
+    private String email;
+
+    private String pwd;
+
+    private String cardId;
+
+    private String sex;
+
+    private String role;
+}
